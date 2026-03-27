8591f9db5b5e470a9e97c9146314ee3753bf8454
khoapham0112 <khoapham1122007@gmail.com>
2026-03-27 08:02:16 +0700

    Optimize Bubble Sort by removing swapped flag
    Removed unnecessary swapped flag and break condition for optimization.

--- Algo/BubbleSort.hpp
+++ Algo/BubbleSort.hpp
@@ -7,11 +7,9 @@ std::pair<double, double> BubbleSort(int a[], int n)
     double compare = 0;
    
     auto start = std::chrono::high_resolution_clock::now();
-    bool swapped = false;
     for (int i = 0; i < n - 1; i++)
     {
         compare++;
-        swapped = false;
         for (int j = 0; j < n - i - 1; j++)
         {
             compare += 2;
@@ -19,15 +17,9 @@ std::pair<double, double> BubbleSort(int a[], int n)
             if (a[j] > a[j + 1])
             {
                 std::swap(a[j], a[j + 1]);
-                swapped = true;
             }
         }
-        
         compare++;
-
-        if (!swapped) {
-            break;
-        }
     }
     compare++;
 
